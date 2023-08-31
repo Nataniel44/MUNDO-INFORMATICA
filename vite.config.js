5443f30c3198cb17d32f7015f44bf2b437da7b28
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-08-31 20:37:42 -0300

    2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MUNDO-INFORMATICA/", // Ruta base relativa
+  base: "https://Nataniel44.github.io/MUNDO-INFORMATICA/", // Ruta base relativa
 });
